5ef910d4135b05e9399e9fad34afc8abfcae96c1
wokePanda <florinusc2@gmail.com>
2019-03-16 12:22:39 +0200

    Added Extensions
    - Added extensions for UITableView and UICollectionView for easier registering and dequeueing of cells
    - Added class ReusableView and NibLoadableView for easier loading of cell views from nibs
    - Conformed UICollectionViewCell and UITableViewCell to ReusableView and NibLoadableView

---
+++ AlbumLover/Helpers/Extensions/UITableViewCell+Extension.swift
@@ -0,0 +1,11 @@
+//
+//  UITableViewCell+Extension.swift
+//  AlbumLover
+//
+//  Created by Florin Uscatu on 16/03/2019.
+//  Copyright © 2019 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell: NibLoadableView, ReusableView {}
---
+++ AlbumLover/Helpers/ReusableView.swift
@@ -0,0 +1,19 @@
+//
+//  ReusableView.swift
+//  AlbumLover
+//
+//  Created by Florin Uscatu on 16/03/2019.
+//  Copyright © 2019 Florin Uscatu. All rights reserved.
+//
+
+import UIKit
+
+protocol ReusableView: class {
+    static var defaultReuseIdentifier: String { get }
+}
+
+extension ReusableView where Self: UIView {
+    static var defaultReuseIdentifier: String {
+        return String(describing: self)
+    }
+}
